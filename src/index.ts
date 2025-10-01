a7e5d27d4b53644fe8af059d1bc954559710f099
Your Name <khalidsaifullaha3@gmail.com>
2025-10-01 13:35:18 +0600

    find data

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,28 @@ import { PrismaClient } from "../generated/prisma/index.js";
 const prisma = new PrismaClient();
 
 async function main() {
-  const result = await prisma.user.create({
-    data: {
-      name: "Khalid Saifullaha",
-      email: "khalid@gmail.com",
-      profilePhoto: "https://khalid.cpm/khalid.png",
+  // const result = await prisma.user.create({
+  //   data: {
+  //     name: "Khalid Saifullaha",
+  //     email: "khalid@gmail.com",
+  //     profilePhoto: "https://khalid.cpm/khalid.png",
+  //   },
+  // });
+  // console.log(result);
+
+  // const usersData = await prisma.user.findMany({
+  //   where: {
+  //     id: 2,
+  //   },
+  // });
+  // console.log(usersData);
+
+  const findUserById = await prisma.user.findUnique({
+    where: {
+      id: 2,
     },
   });
-  console.log(result);
+  console.log(findUserById);
 }
 
 main();
